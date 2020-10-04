35ed99998ab1427d5a90808b7a565b8e43452e88
Siobhan <smgdonnelly@hotmail.com>
2020-10-04 15:56:12 +0100

    Started extracting the main paragraphs.

--- scraper.py
+++ scraper.py
@@ -10,3 +10,10 @@ while current_page is not None:
     response = requests.get(current_page)
     # parse the html using beautiful soup and store as page
     page = bae(response.content, "html.parser")
+
+    main_text = page.find('div', attrs={'class': 'storytextp'})
+    paragraphs = main_text.find('p')
+
+    for paragraph in paragraphs:
+        # TODO Do something interesting here
+        print(paragraph)
